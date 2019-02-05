677cd65a6703e816225b93aba06e4631cdab58ca
Julian <31627984+juli6464@users.noreply.github.com>
2019-02-05 11:07:18 -0500

    Add files via upload

---
+++ grid/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+	To run sample application you need to have:
+	- PHP
+	- MySQL
+	
+	To install application, just make sure that your MySQL running and load index.php 
+	after entering suitable (for you) information below
+*/
+	$mysql_host = "urcryptogroupcom.netfirmsmysql.com";
+	$mysql_user = "urbinauser";
+	$mysql_pasw = "qwerty123";
+	$mysql_db   = "urbina";
+
+?>
