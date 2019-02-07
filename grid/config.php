2da64ce28071c4ebdb2eeeea1ba7a80c622e1ef0
Julian <31627984+juli6464@users.noreply.github.com>
2019-02-07 17:04:23 -0500

    Update config.php

--- grid/config.php
+++ grid/config.php
@@ -8,9 +8,9 @@
 	To install application, just make sure that your MySQL running and load index.php 
 	after entering suitable (for you) information below
 */
-	$mysql_host = "urcryptogroupcom.netfirmsmysql.com";
-	$mysql_user = "urbinauser";
-	$mysql_pasw = "qwerty123";
-	$mysql_db   = "urbina";
+	$mysql_host = "localhost";
+	$mysql_user = "root";
+	$mysql_pasw = "";
+	$mysql_db   = "";
 
 ?>
